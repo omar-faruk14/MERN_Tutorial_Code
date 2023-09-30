ed509f26737ec5159649dc05a1e0d6950e386fc7
Omar Faruk <Omarfaruk885@gmail.com>
2023-09-30 22:48:12 +0900

    Add pages and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
 
           <BrowserRouter>
+          <Navbar />
         <div className="pages">
           <Routes>
             <Route 
-              path="/" 
+              path="/hello" 
               element={<Home />} 
             />
           </Routes>
